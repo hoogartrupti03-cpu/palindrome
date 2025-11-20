9249a8c41489875ad2e83d7fc89cbadc4db148c6
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2025-11-20 14:38:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,13 @@
 import sys
 
-# Program to check if a string is a palindrome
-# Usage:
-# python palindrome.py madam
-
 if len(sys.argv) == 2:
-    text = sys.argv[1]  # Take input from system argument
-    print("User provided input:")
+    text = sys.argv[1]
+    print("User provided input value:")
 else:
-    print("No command-line input provided. Using manual input:")
-    text = "7"
+    print("No input given - using default value:")
+    text = "level"
 
-# Check palindrome
 if text == text[::-1]:
-    print("It is a palindrome.")
+    print(f"'{text}' is a palindrome")
 else:
-    print("It is not a palindrome.")
+    print(f"'{text}' is not a palindrome")
